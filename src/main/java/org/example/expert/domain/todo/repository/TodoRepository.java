1d2dae51cdac19ae948cff6ffe19fbf22e1c3fff
goo3oo <goo3oo0918@gmail.com>
2025-01-18 20:04:23 +0900

    레벨 2-8: QueryDSL

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,9 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryQuerydsl {
 
     @Query("SELECT t FROM Todo t LEFT JOIN t.user u " +
             "WHERE (:startDate IS NULL OR t.modifiedAt >= :startDate) " +
@@ -24,9 +23,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             @Param("endDate") LocalDateTime endDate,
             @Param("weather") String weather
     );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
